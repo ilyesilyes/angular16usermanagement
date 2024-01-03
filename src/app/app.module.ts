a1638f87cd5a8ec611a2927b100b55cbaff345cf
Ilyes FATHALLAH_Meritis <ilyes.fathallah@meritis.fr>
2024-01-03 21:59:57 +0100

    create angular projet achitecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { VerifyComponent } from './component/verify/verify.component';
+import { ResetpasswordComponent } from './component/resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VerifyComponent,
+    ResetpasswordComponent
   ],
   imports: [
     BrowserModule,
